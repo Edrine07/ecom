3eff606be366541460ae371aa43d4763d0102b00
Edrine <edrinepogi123@gmail.com>
2024-10-17 19:23:38 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src="/src/assets/Screenshot 2023-10-18 173328.png" width="full" className="logo" alt="Vite logo" />
+        </div>
+        <div>
+        <img src="/src/assets/Screenshot 2024-10-17 190514.png" width="full" className="logo" alt="Vite logo" />
+        </div>
+    </>
+  )
+}
+
+export default App
